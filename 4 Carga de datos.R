4e3848b8f19f2a2d2138762b720a61c5c6dda4ef
maorjuela <ma.orjuela73@uniandes.edu.co>
2022-10-12 18:16:05 -0500

    Se actualiza el script de carga de datos

--- 4 Carga de datos.R
+++ 4 Carga de datos.R
@@ -1,3 +1,14 @@
+# libreria utils
 x <- read.csv(file = "C:/Users/maorj/Desktop/URosario Taller Data Science en R 2022-10/formas.txt") 
 View(x)
+
+# libreria readr
+library(tidyverse)
+x <- read_csv(file = "C:/Users/maorj/Desktop/URosario Taller Data Science en R 2022-10/formas.txt") 
+View(x)
+
+# carga de archivos Excel
+install.packages("readxl")
+library(readxl)
+
 
